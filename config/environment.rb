02890252f78e5d90b649a95c9a135e4870e32b05
JD Leshan <stritch@gmail.com>
2018-03-20 19:18:39 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -4,21 +4,13 @@ require 'yaml/store'
 require 'ostruct'
 require 'date'
 
-
 require 'bundler/setup'
 Bundler.require
 
 
 ActiveRecord::Base.establish_connection(
-<<<<<<< HEAD
   :adapter => "sqlite3",
   :database => "db/artists.sqlite"
 )
-
-=======
-  :adapter => "sqlite3"
-  :database => "db/artists.sqlite"
-)
->>>>>>> 58e01d90ea6266c83fb212b919796131e4bf86ad
 
 require_relative "../artist.rb"
